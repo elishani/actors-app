1ebdba7ee00830614118e1eb48730415e716e28f
Eli Shani <elishani50@gmail.com>
2020-01-14 14:47:32 +0200

    last changes

--- src/ActorComp.js
+++ src/ActorComp.js
@@ -3,18 +3,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {Card}  from 'react-bootstrap';
 
 class ActorComp extends React.Component {
-    constructor(props) {
-        super(props);
-    }
 
     render() {
-        const actorName=`Name: ${this.props.actor.firstName} ${this.props.actor.lastName}` 
+        console.log(this.props.actor)
+        const actorName=`${this.props.actor.firstName} ${this.props.actor.lastName}` 
         return (
             <div>
                 <Card style={{ width: '18rem' }}>
                     <Card.Img variant="top" src={this.props.actor.image} />
                     <Card.Body>
-                        <Card.Title><a href={this.props.actor.linkActor} target="_blank"> {actorName} </a></Card.Title>
+                        <Card.Title><a href={this.props.actor.link} target="_blank">Name: {actorName} </a>
+                        </Card.Title>
                         <Card.Text>
                             <h4>Age is: {this.props.actor.age()}</h4>
                         </Card.Text>    
--- src/Gallery.js
+++ src/Gallery.js
@@ -1,6 +1,6 @@
 import React from "react";
 import actorsData from "./actorsData.json";
-import ActorObjt from "./ActorModle.js";
+import ActorModle from "./ActorModle.js";
 import ActorComp from "./ActorComp.js";
 import { Row, Container } from 'react-bootstrap';
 
@@ -8,19 +8,48 @@ class Gallery extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            actors: actorsData.map(item => new ActorObjt(item)
+            value: "",
+            actors: actorsData.map(item => new ActorModle(item),
             )
         }
-        
+
+        this.sortSelected = this.sortSelected.bind(this);
     }
 
-    render()
-     {  
-        console.log(actorsData)
-        const ActorComps = this.state.actors.map(actor => <ActorComp actor={actor} />)
-        return (
+    sortSelected(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    render() {
+        const { actors, value } = this.state;
+        const newActors = actors.slice();
+
+        if ( value === "age"){ 
+            newActors.sort(function(a,b){
+                return a.age() - b.age();
+            }); 
+        }
+
+        if ( value === "firstName" || value === "lastName"){
+                const ActorComps = actors.map(actor => <ActorComp actor={actor} />)
+                newActors.sort(function(a,b){
+                let x = a[value].toLowerCase();
+                let y = b[value].toLowerCase();
+                return x < y ? -1 : x > y ? 1 :0;
+            });
+        }  
+
+    const ActorComps = newActors.map(actor => <ActorComp actor={actor} />)
+
+    return (
             <div>
                 <Container>
+                    <select value={value} onChange={this.sortSelected}>
+                        <option value="unSorted">unSorted</option>
+                        <option value="firstName">Sorted By FirstName</option>
+                        <option value="lastName">Sorted By LastName</option>
+                        <option value="age">Sorted By Age</option>
+                    </select>
                     <Row>
                         {ActorComps}
                     </Row>
